d6b89537eb9f225dfb8904e5b8afb62bd67cf57c
sabeer-k-s <newsabeer181@gmail.com>
2024-06-27 12:05:51 +0530

    Training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chickenCNN import logger
 from chickenCNN.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chickenCNN.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chickenCNN.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/chickenCNN/config/configuration.py
+++ src/chickenCNN/config/configuration.py
@@ -3,7 +3,8 @@ from chickenCNN.constants import *
 from chickenCNN.utils.common import read_yaml,create_directories
 from chickenCNN.entity.config_entity import (DataIngestionConfig,
                                              PrepareBaseModelConfig,
-                                             PrepareCallbacksConfig)
+                                             PrepareCallbacksConfig,
+                                             TrainingConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -66,3 +67,26 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config 
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            (training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=(training.root_dir),
+            trained_model_path=(training.trained_model_path),
+            updated_base_model_path=(prepare_base_model.updated_base_model_path),
+            training_data=(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
