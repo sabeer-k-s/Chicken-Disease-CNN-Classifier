09203b94fafd754f9dc7eac42a4b57a67b23e4ed
sabeer-k-s <newsabeer181@gmail.com>
2024-06-28 16:03:33 +0530

    Evaluation added

--- src/chickenCNN/config/configuration.py
+++ src/chickenCNN/config/configuration.py
@@ -4,7 +4,8 @@ from chickenCNN.utils.common import read_yaml,create_directories
 from chickenCNN.entity.config_entity import (DataIngestionConfig,
                                              PrepareBaseModelConfig,
                                              PrepareCallbacksConfig,
-                                             TrainingConfig)
+                                             TrainingConfig,
+                                             EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -90,3 +91,14 @@ class ConfigurationManager:
         )
 
         return training_config
+
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chicken-fecal-images",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
